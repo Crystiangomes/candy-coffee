eb8e46ac61cab9bd0d9ea5aa11d2b47be417c5cc
Crystian Gomes <icrystianbr.com.br@gmail.com>
2026-04-21 14:33:27 -0300

    Integração com banco

--- server.js
+++ server.js
@@ -4,23 +4,15 @@
 ==================================
 */
 
-require("dotenv").config(); // carrega as variáveis do arquivo .env
-const pool = require("./db.js"); // importa a conexão com o banco de dados
+require("dotenv").config(); 
+const pool = require("./db.js");
 
-// 1 - Inporta o Express - ele cria e gerencia o nosso servidor
 const express = require("express");
-
-// 2 - Importa o CORS - permite que o navegador "converse" com o servidor (frontend - backend)
 const cors = require("cors");
 
-// 3 - Cria o servidor (como ligar um computador)
 const app = express();
 
-// 4 - Ativa o CORS - libera a comunicação entre o front-end e o back-end
 app.use(cors());
-
-// 5 - Ativa o leitor de JSON - permite entender os dados recebidos
-// sem isso, o servidor não consegue ler o  que o formulário envia
 app.use(express.json());
 
 
@@ -30,35 +22,122 @@ app.use(express.json());
 ==================================
 */
 
-// 6. Define a rota POST "/mensagem"
-// Quando o form enviar os dados para /mensagem, essa função roda
-app.post("/mensagem", (req,res) => {
-    // 7. req.body contém os dados envciados pelo form(nome,email,mensagem)
+// POST mensagem
+app.post("/mensagem", async (req,res) => {
     try{
         const nome = req.body.nome
         const email = req.body.email
         const mensagem = req.body.mensagem
         
         if(!nome || !email || !mensagem){
-            // codigo 400 - requisição inválida
-            return res.status(400).json({mensagem: "Preecha todos os campos"});
+            return res.status(400).json({mensagem: "Preencha todos os campos"});
         };
 
-        pool.execute("INSERT INTO tb_mensagem(nome,email,mensagem) VALUES(?,?,?)",
-                    [nome,email,mensagem]);
+        await pool.execute(
+            "INSERT INTO tb_mensagem(nome,email,mensagem) VALUES(?,?,?)",
+            [nome,email,mensagem]
+        );
 
-        // codigo 201 - criado com sucesso
         res.status(201).json({mensagem: "Mensagem enviada com sucesso!"});
 
-        // 8. Envia uma resposta de volta para o navegador
-        res.send("Mensagem recebida com sucesso!");
     } catch(error){
         console.error(error);
+        res.status(500).json({ erro: "Erro ao enviar mensagem" });
     }
 });
 
-// 9. Inicia o servidor na porta 3000
-// Depois disso, o servidor fica "ouvindo" por novas mensagens
+// ROTA INICIAL
+app.get("/", (req, res) => {
+    res.send("API Candy Coffee funcionando!");
+});
+
+// GET mensagens
+app.get("/mensagem", async (req, res) => {
+    try {
+        const [rows] = await pool.query("SELECT * FROM tb_mensagem");
+        res.json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ erro: "Erro ao buscar mensagens" });
+    }
+});
+
+// GET produtos
+app.get("/produtos", async (req, res) => {
+    try {
+        const [rows] = await pool.query("SELECT * FROM produtos");
+        res.json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ erro: "Erro ao buscar produtos" });
+    }
+});
+
+// POST produtos
+app.post("/produtos", async (req, res) => {
+    try {
+        const { nome, preco } = req.body;
+
+        if (!nome || !preco) {
+            return res.status(400).json({ mensagem: "Preencha todos os campos" });
+        }
+
+        await pool.execute(
+            "INSERT INTO produtos (nome, preco) VALUES (?, ?)",
+            [nome, preco]
+        );
+
+        res.status(201).json({ mensagem: "Produto criado!" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ erro: "Erro ao criar produto" });
+    }
+});
+
+
+/* 
+==================================
+3 PARTE - INICIAR SERVIDOR
+==================================
+*/
+
 app.listen(3000, ()=>{
     console.log("Servidor rodando em http://localhost:3000");
 });
+
+// UPDATE
+app.put("/produtos/:id", async (req, res) => {
+    try {
+        const { nome, preco } = req.body;
+        const { id } = req.params;
+
+        await pool.execute(
+            "UPDATE produtos SET nome = ?, preco = ? WHERE id = ?",
+            [nome, preco, id]
+        );
+
+        res.json({ mensagem: "Produto atualizado!" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ erro: "Erro ao atualizar produto" });
+    }
+});
+
+//DELETE 
+
+app.delete("/produtos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        await pool.execute(
+            "DELETE FROM produtos WHERE id = ?",
+            [id]
+        );
+
+        res.json({ mensagem: "Produto deletado!" });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ erro: "Erro ao deletar produto" });
+    }
+});
+
